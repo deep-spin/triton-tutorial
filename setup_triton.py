e852a9490b57d95cdf04ca54891d0df07875efbb
Marcos Treviso <marcosvtreviso@gmail.com>
2025-06-16 13:42:26 -0300

    Update notebooks

--- setup_triton.py
+++ setup_triton.py
@@ -1,6 +1,6 @@
 import sys, os
 
-def setup_triton(use_interpreter=True):
+def setup_triton(use_interpreter=True, print_autotuning=False):
     # Add the local triton to your path (MacOS and local installation)
     triton_path = os.path.abspath("triton-cpu/python")
     if os.path.isdir(triton_path) and triton_path not in sys.path:
@@ -10,3 +10,7 @@ def setup_triton(use_interpreter=True):
     # This menas that uou can insert Python breakpoints to debug your kernel code! 
     if use_interpreter:
         os.environ["TRITON_INTERPRET"] = "1"
+
+    # Enable printing of autotuning info: best configuration and benchmark time
+    if print_autotuning:
+        os.environ["TRITON_PRINT_AUTOTUNING"] = "1"
